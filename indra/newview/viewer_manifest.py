6fca86af4b976f74efa5d6c01875967c9417adb9
Techwolf Lupindo <devnull@localhost>
2010-09-11 02:54:29 -0400

    Fix python import error in viewer_manifest.py

--- indra/newview/viewer_manifest.py
+++ indra/newview/viewer_manifest.py
@@ -34,13 +34,11 @@ import sys
 import os.path
 import re
 import tarfile
+import time
 viewer_dir = os.path.dirname(__file__)
 # add llmanifest library to our path so we don't have to muck with PYTHONPATH
-sys.path.append(os.path.join(viewer_dir, '../lib/python'))
-from indra.util.llmanifest import LLManifest
-from indra.util.llmanifest import main
-from indra.util.llmanifest import proper_windows_path
-from indra.util.llmanifest import path_ancestors
+sys.path.append(os.path.join(viewer_dir, '../lib/python/indra/util'))
+from llmanifest import LLManifest, main, proper_windows_path, path_ancestors
 
 class ViewerManifest(LLManifest):
     def is_packaging_viewer(self):
