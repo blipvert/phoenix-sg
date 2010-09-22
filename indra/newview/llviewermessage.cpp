f011e39179c406988f47ff670db74c83b5d6d42d
Kadah <devnull@localhost>
2010-09-21 21:39:47 -0700

    PHOE-368: Ability to set color for Object IMs

--- indra/newview/llviewermessage.cpp
+++ indra/newview/llviewermessage.cpp
@@ -2620,7 +2620,7 @@ void process_improved_im(LLMessageSystem *msg, void **user_data)
 
 			// Note: lie to LLFloaterChat::addChat(), pretending that this is NOT an IM, because
 			// IMs from objcts don't open IM sessions.
-			chat.mSourceType = CHAT_SOURCE_OBJECT;
+			chat.mSourceType = CHAT_SOURCE_OBJECT_IM;
 			LLFloaterChat::addChat(chat, FALSE, FALSE);
 
 			// This message is directly to us, so we should probably always growl?
