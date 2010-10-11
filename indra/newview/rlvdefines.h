bcc8f9a40ae6db34cc4a27d8237544094c6952f9
Kitty Barnett <develop@catznip.com>
2010-10-11 13:39:21 +0200

    Incremented version number to RLVa-1.1.3(c)   -> transplant relevant recent changes from RLVa-1.2.1

--- indra/newview/rlvdefines.h
+++ indra/newview/rlvdefines.h
@@ -76,7 +76,7 @@ const S32 RLV_VERSION_BUILD = 0;
 const S32 RLVa_VERSION_MAJOR = 1;
 const S32 RLVa_VERSION_MINOR = 1;
 const S32 RLVa_VERSION_PATCH = 3;
-const S32 RLVa_VERSION_BUILD = 1;
+const S32 RLVa_VERSION_BUILD = 2;
 
 // Uncomment before a final release
 //#define RLV_RELEASE
